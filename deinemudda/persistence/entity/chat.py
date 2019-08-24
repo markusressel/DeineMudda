0729d0b1df5dfd103634bd5e13d68c45f5f08283
Markus Ressel <mail@markusressel.de>
2019-08-24 04:39:43 +0200

    dont use user_id as primary_key for Voter entities to allow Voter entities for every message

--- deinemudda/persistence/entity/chat.py
+++ deinemudda/persistence/entity/chat.py
@@ -41,10 +41,11 @@ class VoteMenuItemVoter(Base):
     __tablename__ = 'vote_menu_item_voters'
     __table_args__ = (UniqueConstraint('vote_menu_item_id', 'user_id', name='_vote_menu_item_voter_uc'),)
 
-    user_id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     vote_menu_item_id = Column(Integer, ForeignKey('vote_menu_items.id'))
     vote_menu_item = relationship("VoteMenuItem", back_populates="voters")
 
+    user_id = Column(Integer)
     count = Column(Integer)
 
 
