21a3dc4adcc2bce6b3a654940c4d6bd2f484735f
Markus Ressel <mail@markusressel.de>
2019-09-14 02:51:41 +0200

    updated container-app-conf dependency added "word count range" config

--- deinemudda/config.py
+++ deinemudda/config.py
@@ -15,10 +15,11 @@
 
 from container_app_conf import Config
 from container_app_conf.entry.int import IntConfigEntry
+from container_app_conf.entry.range import RangeConfigEntry
 from container_app_conf.entry.string import StringConfigEntry
 
 from deinemudda.const import CONFIG_NODE_ROOT, CONFIG_NODE_TELEGRAM, DEFAULT_SQL_PERSISTENCE_URL, \
-    CONFIG_NODE_PERSISTENCE, CONFIG_NODE_STATS, CONFIG_NODE_PORT
+    CONFIG_NODE_PERSISTENCE, CONFIG_NODE_STATS, CONFIG_NODE_PORT, CONFIG_NODE_BEHAVIOUR, CONFIG_NODE_WORD_COUNT_RANGE
 
 
 class AppConfig(Config):
@@ -42,6 +43,15 @@ class AppConfig(Config):
             "url"
         ],
         default=DEFAULT_SQL_PERSISTENCE_URL)
+
+    WORD_COUNT_RANGE = RangeConfigEntry(
+        yaml_path=[
+            CONFIG_NODE_ROOT,
+            CONFIG_NODE_BEHAVIOUR,
+            CONFIG_NODE_WORD_COUNT_RANGE
+        ],
+        default="[1..10]"
+    )
 
     STATS_PORT = IntConfigEntry(
         yaml_path=[
