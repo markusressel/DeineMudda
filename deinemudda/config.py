cf59acc4eec27fdee0d0de9d80b9e846930b7b79
Markus Ressel <mail@markusressel.de>
2019-10-14 03:50:23 +0200

    updated container-app-conf

--- deinemudda/config.py
+++ deinemudda/config.py
@@ -42,7 +42,7 @@ class AppConfig(ConfigBase):
             CONFIG_NODE_TELEGRAM,
             "bot_token"
         ],
-        none_allowed=False,
+        required=True,
         example="123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11")
 
     SQL_PERSISTENCE_URL = StringConfigEntry(
